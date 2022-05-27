8885dec340f105959ee5be0029a61b2bf4ccba6e
Meherab211 <meherab395@gmail.com>
2022-05-27 21:03:45 +0600

    added readmefile

--- src/pages/MyPortfolio.js
+++ src/pages/MyPortfolio.js
@@ -17,6 +17,44 @@ const MyPortfolio = () => {
 
                 </div>
             </div>
+
+            <div>
+            <h1 className='text-2xl text-primary font '>Skill</h1>
+                <div>
+                  <h2 className="text-xl">Expert:</h2>
+                  <p className="text-success uppercase">html</p>
+                  <p className="text-success uppercase">css3</p>
+                  <p className="text-success uppercase">bootstrap</p>
+                  <p className="text-success uppercase">javascript</p>
+                  <p className="text-success uppercase">e6</p>
+                  <p className="text-success uppercase">reactjs</p>
+                  <p className="text-success uppercase">rest api</p>
+                  <p className="text-success uppercase">json</p>
+                </div>
+                <div>
+                    <h2 className='text-xl'>Familiar:</h2>
+                    <p>NodeJs</p>
+                    <p>Express</p>
+                    <p>MongoDB</p>
+                    <p>Firebase</p>
+                </div>
+                <div>
+                    <h2 className='text-xl'>Tools:</h2>
+                    <p>Github</p>
+                    <p>VSCode</p>
+                    <p>Netlify</p>
+                    <p>Heroku</p>
+                </div>
+                <div className='mt-5'>
+                    <h2 className='text-2xl uppercase'>Education:</h2>
+                   <h4 className='text-xl'>Department of Physics</h4>
+                   <p className='text-sm'>Govt. Titumir College,Mohakhali</p>
+                   <p className='text-sm'>Session:19-2020</p>
+                </div>
+                <div>
+
+                </div>
+            </div>
             <div></div>
         </div>
     );
