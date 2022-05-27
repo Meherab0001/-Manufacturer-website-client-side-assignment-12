17829748947b155d01b4a6c7df52772ee5a4808b
Meherab211 <meherab395@gmail.com>
2022-05-27 21:45:31 +0600

    done

--- src/index.js
+++ src/index.js
@@ -11,10 +11,6 @@ import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-
-
-
   <React.StrictMode>
        <QueryClientProvider client={queryClient}>
        <BrowserRouter>
